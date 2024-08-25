b9b11f4ed7235a34ed98f85d91390646e613a9b0
Belal Elmeligy <belalelmeligy516@gmail.com>
2024-08-25 23:26:50 +0300

    Implement user sigin in functionality

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,23 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserSignupDto } from './dtos/user-signup.dto';
 import { AuthService } from './auth.service';
+import { UserSigninDto } from './dtos/user-signin.dto';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) {}
-    @Post('/signup')
-    async userSignUp(@Body() signupData: UserSignupDto) {
-        const createdUser = await this.authService.userSignUp(signupData);
-        return {
-            success: true,
-            createdUser
-        }
-    }
+  constructor(private authService: AuthService) {}
+  @Post('/signup')
+  async userSignUp(@Body() signupData: UserSignupDto) {
+    const createdUser = await this.authService.userSignUp(signupData);
+    return {
+      success: true,
+      createdUser,
+    };
+  }
+
+  @Post('signin')
+  async userSignin(@Body() signinUser: UserSigninDto) {
+    const accessToken = await this.authService.userSignin(signinUser);
+    return { accessToken };
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
 import { Repository } from 'typeorm';
@@ -16,5 +16,10 @@ export class UsersService {
     } catch (error) {
       throw new BadRequestException();
     }
+  }
+
+  async getUserByUsername(username: string) {
+    const user = await this.repo.findOneBy({ username });
+    return user;
   }
 }
