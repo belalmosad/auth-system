01f82e4de7765bfcf23af5798a93c0577e668ca0
Belal Elmeligy <belalelmeligy516@gmail.com>
2024-09-01 00:11:15 +0300

    Remove unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
@@ -31,7 +29,7 @@ import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
     UsersModule,
     AuthModule
   ],
-  controllers: [AppController],
-  providers: [AppService, JwtAuthGuard]
+  controllers: [],
+  providers: [JwtAuthGuard]
 })
 export class AppModule {}
