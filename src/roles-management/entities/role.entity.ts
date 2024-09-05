c7fd6277325c853b0649d0d401d65116d1a91c08
Belal Elmeligy <belalelmeligy516@gmail.com>
2024-09-05 03:31:59 +0300

    Create role mangemement entity

---
+++ src/roles-management/entities/role.entity.ts
@@ -0,0 +1,9 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name: 'roles'})
+export class RoleEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column()
+    role_name: string;
+}
